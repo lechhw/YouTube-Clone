ab1745a258a0bdc3023aaf651a4cefffe686798c
LeeChangHwan <changhwanlee94@gmail.com>
2022-05-06 17:49:28 +0900

    header menu 버튼 클릭시 side menu 토글기능 구현

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SideMenu from '../side_menu/side_menu';
 import styles from './search_header.module.css';
 
 const SearchHeader = ({ onSearch, onClickLogo }) => {
   const inputRef = React.createRef();
+  const [openSideMenu, setOpenSideMenu] = useState(false);
+
+  const toggleSideMenu = () => {
+    setOpenSideMenu(openSideMenu ? false : true);
+  };
 
   const handleSearch = () => {
     const keyword = inputRef.current.value;
@@ -21,41 +27,55 @@ const SearchHeader = ({ onSearch, onClickLogo }) => {
   };
 
   return (
-    <header className={styles.header}>
-      <div className={styles.logo}>
-        <button className={styles.menu_button} type="button">
-          <i className="fa-solid fa-bars"></i>
-        </button>
-        <img
-          onClick={onClickLogo}
-          className={styles.logo_img}
-          src="./images/logo.png"
-          alt="logo"
-        />
-      </div>
+    <>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <button
+            onClick={toggleSideMenu}
+            className={styles.menu_button}
+            type="button"
+          >
+            <i className="fa-solid fa-bars"></i>
+          </button>
+          <img
+            onClick={onClickLogo}
+            className={styles.logo_img}
+            src="./images/logo.png"
+            alt="logo"
+          />
+        </div>
 
-      <div className={styles.input_group}>
-        <input
-          ref={inputRef}
-          className={styles.input}
-          type="text"
-          placeholder="검색"
-          onKeyPress={onKeyPress}
-        />
-        <button
-          onClick={onClick}
-          className={styles.search_button}
-          type="submit"
-        >
-          <img src="./images/search.svg" alt="search" />
-        </button>
-      </div>
+        <div className={styles.input_group}>
+          <input
+            ref={inputRef}
+            className={styles.input}
+            type="text"
+            placeholder="검색"
+            onKeyPress={onKeyPress}
+          />
+          <button
+            onClick={onClick}
+            className={styles.search_button}
+            type="submit"
+          >
+            <img src="./images/search.svg" alt="search" />
+          </button>
+        </div>
 
-      <button className={styles.login_button}>
-        <i className="fa-regular fa-circle-user"></i>
-        <span className={styles.span}>로그인</span>
-      </button>
-    </header>
+        <button className={styles.login_button}>
+          <i className="fa-regular fa-circle-user"></i>
+          <span className={styles.span}>로그인</span>
+        </button>
+      </header>
+
+      <aside
+        className={`${styles.side_menu} ${
+          openSideMenu ? styles.show : styles.hide
+        }`}
+      >
+        <SideMenu />
+      </aside>
+    </>
   );
 };
 
