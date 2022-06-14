8ed4ac36b684f052d8b5df76618006fe46bff7c8
LeeChangHwan <changhwanlee94@gmail.com>
2022-06-14 15:38:19 +0900

    add channel logo

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -2,24 +2,36 @@ import React from 'react';
 import styles from './video_detail.module.css';
 
 // 비디오 플레이 화면
+const CHANNEL_DEFAULT_IMAGE = './images/channel.png';
 
-const VideoDetail = ({ video, video: { snippet } }) => (
-  <section>
-    <iframe
-      className={styles.video_player}
-      type="text/html"
-      width="100%"
-      height="500px"
-      src={`https://www.youtube.com/embed/${video.id}`}
-      frameBorder="0"
-      allowFullScreen
-    ></iframe>
-    <div className={styles.meta_data}>
-      <h1 className={styles.title}>{snippet.title}</h1>
-      <span className={styles.channel}>{snippet.channelTitle}</span>
-      <pre className={styles.desc}>{snippet.description}</pre>
-    </div>
-  </section>
-);
+const VideoDetail = ({ video, video: { snippet } }) => {
+  const url = snippet.channels || CHANNEL_DEFAULT_IMAGE;
+
+  return (
+    <section>
+      <iframe
+        className={styles.video_player}
+        type="text/html"
+        width="100%"
+        height="500px"
+        src={`https://www.youtube.com/embed/${video.id}`}
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+
+      <div className={styles.wrapper}>
+        <div className={styles.logo}>
+          <img src={url} alt="channel" />
+        </div>
+
+        <div className={styles.meta_data}>
+          <h1 className={styles.title}>{snippet.title}</h1>
+          <span className={styles.channel}>{snippet.channelTitle}</span>
+          <pre className={styles.desc}>{snippet.description}</pre>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default VideoDetail;
--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,9 +1,12 @@
 import React, { memo } from 'react';
 import styles from './video_item.module.css';
 
+const CHANNEL_DEFAULT_IMAGE = './images/channel.png';
 const VideoItem = memo(
   ({ video, video: { snippet }, onClickVideo, display }) => {
     const displayType = display === 'list' ? styles.list : styles.grid;
+    const url = snippet.channels || CHANNEL_DEFAULT_IMAGE;
+
     return (
       <li
         className={`${styles.video} ${displayType}`}
@@ -15,8 +18,17 @@ const VideoItem = memo(
             src={snippet.thumbnails.medium.url}
             alt="thumbnail"
           />
-          <h1 className={styles.title}>{snippet.title}</h1>
-          <span className={styles.channel}>{snippet.channelTitle}</span>
+
+          <div className={styles.wrapper}>
+            <div className={styles.logo}>
+              <img src={url} alt="channel logo" />
+            </div>
+
+            <div className={styles.info}>
+              <h1 className={styles.title}>{snippet.title}</h1>
+              <span className={styles.channel}>{snippet.channelTitle}</span>
+            </div>
+          </div>
         </div>
       </li>
     );
