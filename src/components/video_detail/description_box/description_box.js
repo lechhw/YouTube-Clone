c9608fa7f663ac03da6cda00ac42b08399f1708f
LeeChangHwan <changhwanlee94@gmail.com>
2022-06-15 19:22:26 +0900

    implement toggle open  button and  css styling

---
+++ src/components/video_detail/description_box/description_box.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import styles from './description_box.module.css';
+
+const DescriptionBox = ({ video, video: { snippet } }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className={styles.desc_box}>
+      <pre className={`${styles.desc} ${open ? styles.open : styles.close}`}>
+        {snippet.description}
+      </pre>
+      <button className={styles.button} onClick={() => setOpen(!open)}>
+        {open ? '간략히' : '더보기'}
+      </button>
+    </div>
+  );
+};
+
+export default DescriptionBox;
