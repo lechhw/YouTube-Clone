410becf7f79f38ec4dea62facb2efba4938fb3e2
LeeChangHwan <changhwanlee94@gmail.com>
2022-06-17 19:26:53 +0900

    ♻️ refactor code

--- src/components/search_error/search_error.jsx
+++ src/components/search_error/search_error.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import styles from './search_error.module.css';
 
-const SearchError = ({ darkMode }) => {
-  const mode = darkMode === 'dark' && styles.dark;
+const SearchError = () => {
   return (
-    <section className={`${styles.container} ${mode}`}>
+    <section className={styles.container}>
       <h1>검색 결과가 없습니다.</h1>
       <p>다른 검색어를 입력해 주세요.</p>
     </section>
